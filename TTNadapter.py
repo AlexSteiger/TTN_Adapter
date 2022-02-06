36c206dff00c6758f3038951b951c061d21c918f
AlexSteiger <alexandersteiger89@gmail.com>
2022-02-06 16:56:26 +0100

    ready

--- TTNadapter.py
+++ TTNadapter.py
@@ -6,25 +6,35 @@ import requests
 import pandas as pd
 from sqlalchemy import create_engine
 
+#run the first time only to create the table:
+
+# Before running this script:
+# --pandas version >1.4.0 needs to be installed
+# --create postgresql database 'addferti_lorawan
+#Postgres:
+
+postgreSQLTable = "ard_mrk_wan_1300";
+alchemyEngine   = create_engine('postgresql+psycopg2://postgres:postgres@127.0.0.1/addferti_lorawan', pool_recycle=3600);
+                # create_engine(dialect+driver://username:password@host:port/database)
+
+
 theApplication = "arduino-mrk-wan-1300"
 theAPIKey = "NNSXS.LFKZMYHWIZAXYJQL4TK6PER3CKXX4XNSZULY4EA.YJ7B4GEBZVEDLG6UPTQWVSHL6MMGUVBM4O25XTTBGWGKWO4BUPXA"
 
 # Note the path you have to specify. Double note that it has be prefixed with up.
 theFields = "up.uplink_message.decoded_payload,up.uplink_message.frm_payload"
 
-theNumberOfRecords = 5
+theNumberOfRecords = 11
 
 theURL = "https://eu1.cloud.thethings.network/api/v3/as/applications/" + theApplication + "/packages/storage/uplink_message?order=-received_at&limit=" + str(theNumberOfRecords) + "&field_mask=" + theFields
 
 # These are the headers required in the documentation.
 theHeaders = { 'Accept': 'text/event-stream', 'Authorization': 'Bearer ' + theAPIKey }
 
-print("\n\nFetching from data storage  ...\n")k
+print("\n\nFetching from data storage  ...\n")
 
 r = requests.get(theURL, headers=theHeaders)
-print(type(r))
-print(r.text)
-"""
+#print(r.text)
 
 print("URL: " + r.url)
 print()
@@ -35,10 +45,12 @@ print()
 #print(r.text)
 #print()
 
-# Due to some design choices by TTI, the text returned is not proper
-# JSON. The next line fixes that
+# Due to some design choices by TTI, the text returned is not proper JSON.
+# The next line fixes that
 theJSON = "{\"data\": [" + r.text.replace("\n\n", ",")[:-1] + "]}";
 
+# Block to write and read the JSON to and from a file 
+"""
 parsedJSON = json.loads(theJSON)
 
 with open('TTNresponse.json', 'w') as f:
@@ -46,30 +58,26 @@ with open('TTNresponse.json', 'w') as f:
 
 print(theNumberOfRecords, "records fetched from TTN")
 
-########################################################################
-########################################################################
-########################################################################
-########################################################################
-
 df = pd.read_json('TTNresponse.json')
 print("number of rows recieved: ",len(df))
-print()
+"""
+
+df = pd.read_json(theJSON)
 
 # solution from https://stackoverflow.com/questions/45325208/python-json-normalize-a-pandas-series-gives-typeerror
 normalized_df = pd.concat([pd.DataFrame(pd.json_normalize(x)) for x in df['data']],ignore_index=True)
 
 #print("column headers:")
-#for col in normalized_df.columns:
-#  print(col)
-  #-------------------------------------------
-  #result.end_device_ids.device_id
-  #result.received_at
-  #result.uplink_message.frm_payload
-  #result.uplink_message.decoded_payload.relative_humidity_2
-  #result.uplink_message.decoded_payload.temperature_1
-  #result.uplink_message.received_at
-  #-------------------------------------------
-print()
+	#for col in normalized_df.columns:
+	#  print(col)
+	#-------------------------------------------
+	#result.end_device_ids.device_id
+	#result.received_at
+	#result.uplink_message.frm_payload
+	#result.uplink_message.decoded_payload.relative_humidity_2
+	#result.uplink_message.decoded_payload.temperature_1
+	#result.uplink_message.received_at
+	#-------------------------------------------
 
 # subset of the normalized dataframe
 df = normalized_df[[
@@ -87,26 +95,17 @@ TTN_df = df.rename(columns={
 print("Fetched data: ")
 print(TTN_df)
 
-# create_engine(dialect+driver://username:password@host:port/database)
-alchemyEngine = create_engine('postgresql+psycopg2://postgres:postgres@127.0.0.1/json', pool_recycle=3600);
-
 postgreSQLConnection = alchemyEngine.connect();
 
-postgreSQLTable      = "ard_mrk_wan_1300";
-
-#run the first time only to create the table:
-#frame = TTN_df.to_sql(postgreSQLTable, postgreSQLConnection, if_exists='fail');
-
-psql_df = pd.read_sql('select * from ard_mrk_wan_1300', con=postgreSQLConnection)
-
-psql_df = psql_df.drop(columns=['index'])
-
-df_unique = pd.concat([TTN_df,psql_df]).drop_duplicates(keep=False)
-
-print(len(df_unique), "new rows added to the database")
-
-frame = df_unique.to_sql(postgreSQLTable, postgreSQLConnection, if_exists='append');
-
-postgreSQLConnection.close();
-"""
+try:
+  psql_df = pd.read_sql('select * from ard_mrk_wan_1300', con=postgreSQLConnection)
+  psql_df = psql_df.drop(columns=['index'])
+  df_unique = pd.concat([TTN_df,psql_df]).drop_duplicates(keep=False)
+  print(len(df_unique), "new rows added to the database")
+  frame = df_unique.to_sql(postgreSQLTable, postgreSQLConnection, if_exists='append');
+except:
+  print("create table", postgreSQLTable)
+  frame = TTN_df.to_sql(postgreSQLTable, postgreSQLConnection, if_exists='fail');
+finally:
+  postgreSQLConnection.close();  
 
